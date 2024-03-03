4cd453f97121681abba49c6cf9873d7670a29ca0
Scott Morse <scott.m.morse@gmail.com>
2024-03-03 00:58:11 +0000

    testing sendfile

--- myApp.js
+++ myApp.js
@@ -1,12 +1,12 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-
+absolutePath = __dirname + '/views/index.html'
 app.get("/", getExpress);
 
 
 function getExpress(req, res){
-    res.send('Hello Express');
+    res.sendFile(absolutePath);
   }
 
 
