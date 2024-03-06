ef5365f64b4db7c6042cc914c270f62574e9e089
Scott Morse <scott.m.morse@gmail.com>
2024-03-06 01:05:41 +0000

    added double quotes

--- myApp.js
+++ myApp.js
@@ -4,9 +4,9 @@ console.log("Hello World");
 absolutePath = __dirname + '/views/index.html'
 app.get("/", loadIndex);
 
-publicPath = __dirname + '/public'
+publicPath = __dirname + "/public"
 
-app.use('/public',express.static(publicPath));
+app.use("/public",express.static(publicPath));
 
 
 
