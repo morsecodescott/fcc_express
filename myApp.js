848b2b52aea2b7f7d044be37f895a42a26bae007
Scott Morse <scott.m.morse@gmail.com>
2024-03-06 01:01:25 +0000

    app.use

--- myApp.js
+++ myApp.js
@@ -4,6 +4,10 @@ console.log("Hello World");
 absolutePath = __dirname + '/views/index.html'
 app.get("/", loadIndex);
 
+publicPath = __dirname + '/public'
+
+app.use(express.static(publicPath));
+
 
 
 
