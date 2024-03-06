3044667ad23210e333b710d8d1cc9df175447034
Scott Morse <scott.m.morse@gmail.com>
2024-03-06 01:44:30 +0000

    added json

--- myApp.js
+++ myApp.js
@@ -2,13 +2,18 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 absolutePath = __dirname + '/views/index.html'
-app.get("/", loadIndex);
-
 publicPath = __dirname + "/public"
 
+
+app.get("/", loadIndex);
 app.use("/public",express.static(publicPath));
+app.get("/json", loadJson)
 
 
+
+function loadJson(req,res){
+  res.json({"message": "Hello json"});
+}
 
 
 
