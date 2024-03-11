32ca03b5fed3ec2bb8682983558334ba01e5afb8
Scott Morse <scott.m.morse@gmail.com>
2024-03-11 23:58:58 +0000

    test

--- myApp.js
+++ myApp.js
@@ -4,27 +4,23 @@ console.log("Hello World");
 absolutePath = __dirname + '/views/index.html'
 publicPath = __dirname + "/public"
 
-
 app.get("/", loadIndex);
 app.use("/public",express.static(publicPath));
 app.get("/json", loadJson);
 
-MESSAGE_STYLE = "uppercase";
 
 function loadJson(req,res){
   jsonText = {"message": "Hello json"}
-  console.log(process.env);
+  console.log(process.env.MESSAGE_STYLE);
   
-  if (MESSAGE_STYLE === "uppercase"){
+  if (process.env.MESSAGE_STYLE === "uppercase"){
     jsonText.message = jsonText.message.toUpperCase();
     console.log("uppercase");
     res.json(jsonText);
   }else{
     console.log("lower case");
     res.json(jsonText);
-  }
-  
-  
+  }  
 }
 
 
